438bcf70a6f35802ea175ae13f828901d8aa205f
RomelOrtega <romel_ortega@ucne.edu.do>
2026-06-20 19:20:56 -0400

    Crea GetPlanetDetailUseCase

---
+++ app/src/main/java/edu/ucne/planetapi/domain/usecase/GetPlanetDetailUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.planetapi.domain.usecase
+
+import edu.ucne.planetapi.domain.repository.PlanetRepository
+import javax.inject.Inject
+
+class GetPlanetDetailUseCase @Inject constructor(
+    private val repository: PlanetRepository
+) {
+    suspend operator fun invoke(id: Int) = repository.getPlanetById(id)
+}
