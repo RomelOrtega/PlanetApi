9c18a0aeccb953c823787be617fc8b2985669d61
RomelOrtega <romel_ortega@ucne.edu.do>
2026-06-20 18:45:11 -0400

    Crea PlanetDto

---
+++ app/src/main/java/edu/ucne/planetapi/data/remote/dto/PlanetDto.kt
@@ -0,0 +1,23 @@
+package edu.ucne.planetapi.data.remote.dto
+
+import edu.ucne.planetapi.domain.model.Planet
+
+data class PlanetsResponse(
+    val items: List<PlanetDto>
+)
+
+data class PlanetDto(
+    val id: Int,
+    val name: String,
+    val isDestroyed: Boolean,
+    val description: String,
+    val image: String
+) {
+    fun toDomain() = Planet(
+        id = id,
+        name = name,
+        isDestroyed = isDestroyed,
+        description = description,
+        image = image
+    )
+}
