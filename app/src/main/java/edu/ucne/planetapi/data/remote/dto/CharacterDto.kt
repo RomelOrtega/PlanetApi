5100c55afd0db735181fa2e3ead02c7517900ce2
RomelOrtega <romel_ortega@ucne.edu.do>
2026-06-27 17:35:46 -0400

    Crea CharacterDto

---
+++ app/src/main/java/edu/ucne/planetapi/data/remote/dto/CharacterDto.kt
@@ -0,0 +1,22 @@
+package edu.ucne.planetapi.data.remote.dto
+
+import edu.ucne.planetapi.domain.model.Character
+
+data class CharacterResponseDto(
+    val items: List<CharacterDto>
+)
+
+data class CharacterDto(
+    val id: Int,
+    val name: String,
+    val ki: String,
+    val race: String,
+    val gender: String,
+    val description: String,
+    val image: String,
+    val maxKi: String,
+) {
+    fun toDomain() = Character(
+        id, name, ki, race, gender, description, image, maxKi,
+    )
+}
