f02917ed877d50ffdd14ff335e88dae953e8d444
RomelOrtega <romel_ortega@ucne.edu.do>
2026-06-27 18:06:34 -0400

    Crea GetCharacterDetailUseCase

---
+++ app/src/main/java/edu/ucne/planetapi/domain/usecase/GetCharacterDetailUseCase.kt
@@ -0,0 +1,10 @@
+package edu.ucne.planetapi.domain.usecase
+
+import edu.ucne.planetapi.domain.repository.CharacterRepository
+import javax.inject.Inject
+
+class GetCharacterDetailUseCase @Inject constructor(
+    private val repository: CharacterRepository
+) {
+    operator fun invoke(id: Int) = repository.getCharacterDetail(id)
+}
