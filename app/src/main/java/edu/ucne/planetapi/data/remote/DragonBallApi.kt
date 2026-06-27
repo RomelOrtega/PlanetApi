adfe9739a70d56deffee2a18537fe4969fdeb5a3
RomelOrtega <romel_ortega@ucne.edu.do>
2026-06-27 17:48:16 -0400

    Modifica el DragonBallApi

--- app/src/main/java/edu/ucne/planetapi/data/remote/DragonBallApi.kt
+++ app/src/main/java/edu/ucne/planetapi/data/remote/DragonBallApi.kt
@@ -2,6 +2,8 @@ package edu.ucne.planetapi.data.remote
 
 import edu.ucne.planetapi.data.remote.dto.PlanetDto
 import edu.ucne.planetapi.data.remote.dto.PlanetResponseDto
+import edu.ucne.planetapi.data.remote.dto.CharacterResponseDto
+import edu.ucne.planetapi.data.remote.dto.CharacterDto
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -20,4 +22,18 @@ interface DragonBallApi {
     suspend fun getPlanetDetail(
         @Path("id") id: Int
     ): Response<PlanetDto>
+
+    @GET("characters")
+    suspend fun getCharacters(
+        @Query("page") page: Int,
+        @Query("limit") limit: Int,
+        @Query("name") name: String?,
+        @Query("gender") gender: String?,
+        @Query("race") race: String?
+    ): Response<CharacterResponseDto>
+
+    @GET("characters/{id}")
+    suspend fun getCharacterDetail(
+        @Path("id") id: Int
+    ): Response<CharacterDto>
 }
